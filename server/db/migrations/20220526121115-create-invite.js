5daeae0d378ae341902b3c204e9527b4de53dea4
Nikolay <nikogorichev812@gmail.com>
2022-05-26 16:12:02 +0300

    add datebase again

---
+++ server/db/migrations/20220526121115-create-invite.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Invites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      event_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Events',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Invites');
+  }
+};
---
+++ server/db/seeders/20220526121540-users.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'admin',
+        email: 'admin@mail.ru',
+        password: 'admin',
+        description: 'i am admin',
+        isAdmin: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('People', null, {});
+  },
+};
