8f99c519106b6ab0785662715161dc08813512e8
Liza <doroshenko.liza@bk.ru>
2022-05-26 15:04:19 +0300

    add skelet back

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// dotenv (npm i dotenv) подключается также в .sequelizerc
+require('dotenv').config();
+const express = require('express');
+const config = require('./config/config');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 4000;
+
+config(app);
+
+// app.get('/', (req, res) => {
+//   res.send('VSE OK');
+// });
+
+app.listen(PORT, () => {
+  console.log(`***Server started at ${PORT} PORT***`);
+});
