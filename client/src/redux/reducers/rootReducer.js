cde559eff922ce07b43136c9b9f6f3862baa0a9a
Olesya <olboyarshinova@gmail.com>
2022-05-29 12:55:39 +0300

    edit profile

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import { userReducer } from './userReducer'
 import { usersSportsReducer } from './usersSportsReducer'
 import { eventsReducer } from './eventsReducer'
-import { userReducer } from './userReducer'
 
 export const rootReducer = combineReducers({
   user: userReducer,
