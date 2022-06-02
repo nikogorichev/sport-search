0aa3872f9f6faaa765325aa67ea2e03b7d2574a4
Liza <doroshenko.liza@bk.ru>
2022-06-02 16:31:02 +0300

    modals

--- client/src/components/OpenModal/OpenModal.jsx
+++ client/src/components/OpenModal/OpenModal.jsx
@@ -3,6 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAddEvents } from "../../redux/thunk/asyncEvents";
 import {useNavigate} from 'react-router-dom'
 import axios from "axios";
+import { Box, Button, Modal, styled, Typography, Input, InputLabel, Select, MenuItem, FormControl, TextField, InputAdornment, } from '@mui/material'
+import { margin, style } from "@mui/system";
+import { AccountCircle, AddCircleOutline, GroupAdd } from "@mui/icons-material";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+// const StyledBox = styled(Box)({
+//   display: "flex",
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// })
 
 const OpenModal = ({ active, setActive }) => {
   const [imageURL, setImageUrl] = useState(null);
@@ -44,47 +59,166 @@ const OpenModal = ({ active, setActive }) => {
     setActive(false)
   }
 
+  const MyButton = styled(Button)(({ theme }) => ({
+    borderRadius: "20px",
+    backgroundColor: "#1a237e",
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+  }));
+
   return (
-    <div className={active ? "modal active" : "modal"}
-      onClick={() => setActive(false)}>
-      <form
-        //ФУНКЦИЯ ДЛЯ ОТПРАВКИ ФОРМЫ ДОБАВЛЕНИЯ МЕРОПРИЯТИЯ
-        onSubmit={newEvent}
-        className="modal__content"
-        onClick={(e) => e.stopPropagation()}
+    // <div className={active ? "modal active" : "modal"}
+    //   onClick={() => setActive(false)}>
+    //   <form
+    //     //ФУНКЦИЯ ДЛЯ ОТПРАВКИ ФОРМЫ ДОБАВЛЕНИЯ МЕРОПРИЯТИЯ
+    //     onSubmit={newEvent}
+    //     className="modal__content"
+    //     onClick={(e) => e.stopPropagation()}
+    //   >
+    //     <h5>Добавить событие:</h5>
+    //     <p class="card-title">Название:</p>
+    //     <input type="text" name="title" />
+    //     <p class="card-text">Дата мероприятия:</p>
+    //     <input type="datetime-local" name="date" />
+    //     <p class="card-text">Описание:</p>
+    //     <input type="text" name="description" />
+    //     <p class="card-text">Количество участников:</p>
+    //     <input type="text" name="members_count" />
+    //     <p class="card-text">Способ связи:</p>
+    //     <input type="text" name="phone" />
+    //     <p class="card-text">Вид спорта:</p>
+    //     <select name="sport_id">
+    //       {sports.map((el) => <option>{ el.title }</option>)}
+    //     </select>
+    //     <p class="card-text">Место проведения:</p>
+    //     <select name="place_id">
+    //       {places.map((el) => <option>{el.title}</option>)}
+    //     </select>
+    //     <p class="card-text">Стоимость:</p>
+    //     <input type="text" name="cost" defaultValue={0}/>
+    //     <input
+    //         id="img"
+    //         type="file"
+    //         onChange={(e) => handleImageUpload(e, setImageUrl)}
+    //       />
+    //     <button type="submit">
+    //       Ответить
+    //     </button>
+    //   </form>
+    // </div>
+<>
+    {/* <Button onClick={handleOpen}>Open modal</Button> */}
+<StyledModal
+  open={active}
+  onClose={e => setActive(false)}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
       >
-        <h5>Добавить событие:</h5>
-        <p class="card-title">Название:</p>
-        <input type="text" name="title" />
-        <p class="card-text">Дата мероприятия:</p>
-        <input type="datetime-local" name="date" />
-        <p class="card-text">Описание:</p>
-        <input type="text" name="description" />
-        <p class="card-text">Количество участников:</p>
-        <input type="text" name="members_count" />
-        <p class="card-text">Способ связи:</p>
-        <input type="text" name="phone" />
-        <p class="card-text">Вид спорта:</p>
-        <select name="sport_id">
-          {sports.map((el) => <option>{ el.title }</option>)}
-        </select>
-        <p class="card-text">Место проведения:</p>
-        <select name="place_id">
-          {places.map((el) => <option>{el.title}</option>)}
-        </select>
-        <p class="card-text">Стоимость:</p>
-        <input type="text" name="cost" defaultValue={0}/>
-        <input
+        
+      <form
+    //ФУНКЦИЯ ДЛЯ ОТПРАВКИ ФОРМЫ ДОБАВЛЕНИЯ МЕРОПРИЯТИЯ
+    onSubmit={newEvent}
+    // className="modal__content"
+    onClick={(e) => e.stopPropagation()}
+    >
+          <Box
+            // paddingLeft={15}
+            // paddingTop={8}
+            display={"flex"}
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={800} height={700} bgcolor={'white'} borderRadius={4} >
+    <Typography marginBottom={2} id="modal-modal-title" variant="h6" component="h2">
+      Добавить событие:
+            </Typography>
+            <TextField
+              multiline
+              label='Название'
+              size="small"
+              style={{width:'80%', margin: '5px', }}
+              type="text" name="title" />
+            
+            <TextField
+              label='Дата мероприятия'
+              size="small"
+              style={{ width: '80%', margin: '5px', }}
+              type="datetime-local" name="date" />
+            <TextField
+              multiline
+              label='Описание'
+              size="small"
+              style={{ width: '80%', margin: '5px', }}
+              type="text" name="description" />
+            <TextField
+              multiline
+              label='Количество участников'
+              size="small"
+              style={{ width: '80%', margin: '5px', }}
+              type="text" name="members_count" />
+            <TextField
+              multiline
+              label='Способ связи'
+              size="small"
+              style={{ width: '80%', margin: '5px', }}
+              type="text" name="phone" />
+            <Box
+              display={"flex"}
+              flexDirection={'row'}
+              // alignItems={'center'}
+              justifyContent={'row'}
+            >
+              <Box>
+            <InputLabel >Вид спорта:</InputLabel>
+            <FormControl>
+                  <Select
+                
+                style={{ width: '310px', margin: '5px', }}
+                size="small" name="sport_id">
+                {sports.map((el) => <MenuItem value={el.title}>{el.title}</MenuItem>)}
+              </Select>
+                </FormControl>
+              </Box>
+              <Box>
+            <InputLabel >Место проведения:</InputLabel>
+            <FormControl>
+              <Select
+                style={{ width: '310px', margin: '5px', }}
+                size="small" name="place_id" label='Место' >
+              {places.map((el) => <MenuItem value={el.title}>{el.title}</MenuItem>)}
+            </Select>
+                </FormControl>
+              </Box>
+            </Box>
+            <TextField
+              multiline
+              label='Стоимость'
+              size="small"
+              style={{ width: '80%', margin: '5px', }}
+              type="text" name="cost" defaultValue={0} />
+            <br/>
+            <TextField
+            size="small"
+              style={{ width: '80%', margin: '5px', marginBottom: '25px', }}
             id="img"
             type="file"
-            onChange={(e) => handleImageUpload(e, setImageUrl)}
-          />
-        <button type="submit">
-          Ответить
-        </button>
-      </form>
-    </div>
+             onChange={(e) => handleImageUpload(e, setImageUrl)}
+            />
+            <br/>
+            <MyButton
+              type="submit"
+              variant="contained"
+              startIcon={<AddCircleOutline />}
+            >
+              ДОБАВИТЬ
+            </MyButton>
+          </Box>
+    </form>
+      </StyledModal>
+    </>
   );
+
 };
 
 export default OpenModal;
